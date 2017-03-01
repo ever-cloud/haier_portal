64195fc05644a490e684c5e55da1f35c6e0aeee1
zhangyou170227 <13269064076@163.com>
2017-03-01 10:56:15 +0800

    haier修改版本
    左侧导航栏及代码整理

---
+++ html/js/common/setcookie.js
@@ -0,0 +1,21 @@
+/*设置cookie*/
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+/*读取cookie*/
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+function reload(){
+    var html = getCookie("html");
+    //console.log(html);
+    var item = getCookie("name");
+    //console.log(item);
+}
